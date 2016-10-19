ee34c372ca9820fdd5614b6c06ed76dfbbd3ae3c
Eric Fletcher <eric.fletcher@noaa.gov>
2016-10-18 16:44:35 -1000

    Added Util.cs.

---
+++ Util.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGEPRO.GUI
+{
+    class Util
+    {
+        static public string GetAgeproUserDataPath()
+        {
+            string appDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            appDir = System.IO.Path.Combine(appDir, System.Windows.Forms.Application.ProductName);
+            if (!System.IO.Directory.Exists(appDir))
+            {
+                System.IO.Directory.CreateDirectory(appDir);
+            }
+            return appDir;
+        }
+
+        //create temp
+    }
+}
