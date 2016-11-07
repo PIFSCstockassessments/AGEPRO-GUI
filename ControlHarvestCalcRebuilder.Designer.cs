5a0905db67c832eccd41710a28042a085527cc7f
Eric Fletcher <eric.fletcher@noaa.gov>
2016-11-07 09:20:14 -1000

    Set Minimum size, not Maximum size

--- ControlHarvestCalcRebuilder.Designer.cs
+++ ControlHarvestCalcRebuilder.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.groupRebuilderTargetParameters = new System.Windows.Forms.GroupBox();
-            this.labelRebuilderTargetYear = new System.Windows.Forms.Label();
-            this.labelRebuilderTargetBiomass = new System.Windows.Forms.Label();
-            this.labelRebuilderTargetPercent = new System.Windows.Forms.Label();
-            this.labelRebuilderTargetType = new System.Windows.Forms.Label();
-            this.textBoxRebuilderTargetYear = new System.Windows.Forms.TextBox();
-            this.textBoxRebuilderTargetBiomass = new System.Windows.Forms.TextBox();
-            this.textBoxRebuilderTargetPercent = new System.Windows.Forms.TextBox();
             this.comboBoxRebuilderTargetType = new System.Windows.Forms.ComboBox();
+            this.textBoxRebuilderTargetPercent = new System.Windows.Forms.TextBox();
+            this.textBoxRebuilderTargetBiomass = new System.Windows.Forms.TextBox();
+            this.textBoxRebuilderTargetYear = new System.Windows.Forms.TextBox();
+            this.labelRebuilderTargetType = new System.Windows.Forms.Label();
+            this.labelRebuilderTargetPercent = new System.Windows.Forms.Label();
+            this.labelRebuilderTargetBiomass = new System.Windows.Forms.Label();
+            this.labelRebuilderTargetYear = new System.Windows.Forms.Label();
             this.groupRebuilderTargetParameters.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,32 +57,34 @@
             this.groupRebuilderTargetParameters.TabStop = false;
             this.groupRebuilderTargetParameters.Text = "Rebuilder Specificatiion";
             // 
-            // labelRebuilderTargetYear
+            // comboBoxRebuilderTargetType
             // 
-            this.labelRebuilderTargetYear.AutoSize = true;
-            this.labelRebuilderTargetYear.Location = new System.Drawing.Point(29, 46);
-            this.labelRebuilderTargetYear.Name = "labelRebuilderTargetYear";
-            this.labelRebuilderTargetYear.Size = new System.Drawing.Size(111, 13);
-            this.labelRebuilderTargetYear.TabIndex = 0;
-            this.labelRebuilderTargetYear.Text = "Rebuilder Target Year";
+            this.comboBoxRebuilderTargetType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRebuilderTargetType.Location = new System.Drawing.Point(217, 159);
+            this.comboBoxRebuilderTargetType.Name = "comboBoxRebuilderTargetType";
+            this.comboBoxRebuilderTargetType.Size = new System.Drawing.Size(186, 21);
+            this.comboBoxRebuilderTargetType.TabIndex = 7;
             // 
-            // labelRebuilderTargetBiomass
+            // textBoxRebuilderTargetPercent
             // 
-            this.labelRebuilderTargetBiomass.AutoSize = true;
-            this.labelRebuilderTargetBiomass.Location = new System.Drawing.Point(29, 84);
-            this.labelRebuilderTargetBiomass.Name = "labelRebuilderTargetBiomass";
-            this.labelRebuilderTargetBiomass.Size = new System.Drawing.Size(111, 13);
-            this.labelRebuilderTargetBiomass.TabIndex = 1;
-            this.labelRebuilderTargetBiomass.Text = "Rebuilder Target (MT)";
+            this.textBoxRebuilderTargetPercent.Location = new System.Drawing.Point(217, 118);
+            this.textBoxRebuilderTargetPercent.Name = "textBoxRebuilderTargetPercent";
+            this.textBoxRebuilderTargetPercent.Size = new System.Drawing.Size(186, 20);
+            this.textBoxRebuilderTargetPercent.TabIndex = 6;
             // 
-            // labelRebuilderTargetPercent
+            // textBoxRebuilderTargetBiomass
             // 
-            this.labelRebuilderTargetPercent.AutoSize = true;
-            this.labelRebuilderTargetPercent.Location = new System.Drawing.Point(29, 121);
-            this.labelRebuilderTargetPercent.Name = "labelRebuilderTargetPercent";
-            this.labelRebuilderTargetPercent.Size = new System.Drawing.Size(149, 13);
-            this.labelRebuilderTargetPercent.TabIndex = 2;
-            this.labelRebuilderTargetPercent.Text = "Rebuilder Percent Confidence";
+            this.textBoxRebuilderTargetBiomass.Location = new System.Drawing.Point(217, 81);
+            this.textBoxRebuilderTargetBiomass.Name = "textBoxRebuilderTargetBiomass";
+            this.textBoxRebuilderTargetBiomass.Size = new System.Drawing.Size(186, 20);
+            this.textBoxRebuilderTargetBiomass.TabIndex = 5;
+            // 
+            // textBoxRebuilderTargetYear
+            // 
+            this.textBoxRebuilderTargetYear.Location = new System.Drawing.Point(217, 43);
+            this.textBoxRebuilderTargetYear.Name = "textBoxRebuilderTargetYear";
+            this.textBoxRebuilderTargetYear.Size = new System.Drawing.Size(186, 20);
+            this.textBoxRebuilderTargetYear.TabIndex = 4;
             // 
             // labelRebuilderTargetType
             // 
@@ -93,41 +95,39 @@
             this.labelRebuilderTargetType.TabIndex = 3;
             this.labelRebuilderTargetType.Text = "Rebuilder Target Type";
             // 
-            // textBoxRebuilderTargetYear
+            // labelRebuilderTargetPercent
             // 
-            this.textBoxRebuilderTargetYear.Location = new System.Drawing.Point(217, 43);
-            this.textBoxRebuilderTargetYear.Name = "textBoxRebuilderTargetYear";
-            this.textBoxRebuilderTargetYear.Size = new System.Drawing.Size(186, 20);
-            this.textBoxRebuilderTargetYear.TabIndex = 4;
+            this.labelRebuilderTargetPercent.AutoSize = true;
+            this.labelRebuilderTargetPercent.Location = new System.Drawing.Point(29, 121);
+            this.labelRebuilderTargetPercent.Name = "labelRebuilderTargetPercent";
+            this.labelRebuilderTargetPercent.Size = new System.Drawing.Size(149, 13);
+            this.labelRebuilderTargetPercent.TabIndex = 2;
+            this.labelRebuilderTargetPercent.Text = "Rebuilder Percent Confidence";
             // 
-            // textBoxRebuilderTargetBiomass
+            // labelRebuilderTargetBiomass
             // 
-            this.textBoxRebuilderTargetBiomass.Location = new System.Drawing.Point(217, 81);
-            this.textBoxRebuilderTargetBiomass.Name = "textBoxRebuilderTargetBiomass";
-            this.textBoxRebuilderTargetBiomass.Size = new System.Drawing.Size(186, 20);
-            this.textBoxRebuilderTargetBiomass.TabIndex = 5;
+            this.labelRebuilderTargetBiomass.AutoSize = true;
+            this.labelRebuilderTargetBiomass.Location = new System.Drawing.Point(29, 84);
+            this.labelRebuilderTargetBiomass.Name = "labelRebuilderTargetBiomass";
+            this.labelRebuilderTargetBiomass.Size = new System.Drawing.Size(111, 13);
+            this.labelRebuilderTargetBiomass.TabIndex = 1;
+            this.labelRebuilderTargetBiomass.Text = "Rebuilder Target (MT)";
             // 
-            // textBoxRebuilderTargetPercent
+            // labelRebuilderTargetYear
             // 
-            this.textBoxRebuilderTargetPercent.Location = new System.Drawing.Point(217, 118);
-            this.textBoxRebuilderTargetPercent.Name = "textBoxRebuilderTargetPercent";
-            this.textBoxRebuilderTargetPercent.Size = new System.Drawing.Size(186, 20);
-            this.textBoxRebuilderTargetPercent.TabIndex = 6;
-            // 
-            // comboBoxRebuilderTargetType
-            // 
-            this.comboBoxRebuilderTargetType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxRebuilderTargetType.Location = new System.Drawing.Point(217, 159);
-            this.comboBoxRebuilderTargetType.Name = "comboBoxRebuilderTargetType";
-            this.comboBoxRebuilderTargetType.Size = new System.Drawing.Size(186, 21);
-            this.comboBoxRebuilderTargetType.TabIndex = 7;
+            this.labelRebuilderTargetYear.AutoSize = true;
+            this.labelRebuilderTargetYear.Location = new System.Drawing.Point(29, 46);
+            this.labelRebuilderTargetYear.Name = "labelRebuilderTargetYear";
+            this.labelRebuilderTargetYear.Size = new System.Drawing.Size(111, 13);
+            this.labelRebuilderTargetYear.TabIndex = 0;
+            this.labelRebuilderTargetYear.Text = "Rebuilder Target Year";
             // 
             // ControlHarvestCalcRebuilder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.groupRebuilderTargetParameters);
-            this.MaximumSize = new System.Drawing.Size(480, 400);
+            this.MinimumSize = new System.Drawing.Size(480, 400);
             this.Name = "ControlHarvestCalcRebuilder";
             this.Size = new System.Drawing.Size(480, 400);
             this.groupRebuilderTargetParameters.ResumeLayout(false);
