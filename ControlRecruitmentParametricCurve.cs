40777e112ac27e48e402df7c0d0e6c4b6f5b8a7d
Eric Fletcher <eric.fletcher@noaa.gov>
2017-02-10 10:48:11 -1000

    Begin implementing a way to input nullable double values that is consistent as non-nullables:
    -Reject blank and non-numeric values and revert/undo to previous value.

--- ControlRecruitmentParametricCurve.cs
+++ ControlRecruitmentParametricCurve.cs
@@ -51,7 +51,7 @@ namespace Nmfs.Agepro.Gui
                 this.textBoxKParm.DataBindings.Add("Text", currentParametricCurveRecruit, "kParm", true,
                     DataSourceUpdateMode.OnPropertyChanged);
             }
-
+            
             if (currentParametricCurveRecruit.autocorrelated)
             {
                 this.labelPhi.Enabled = true;
@@ -59,13 +59,76 @@ namespace Nmfs.Agepro.Gui
                 this.textBoxPhi.Enabled = true;
                 this.textBoxLastResidual.Enabled = true;
 
-                this.textBoxPhi.DataBindings.Add("Text", currentParametricCurveRecruit, "phi", true, 
-                    DataSourceUpdateMode.OnPropertyChanged);
+                Binding b =  new Binding("Text", currentParametricCurveRecruit, "phi", true);
+                b.Format -= new ConvertEventHandler(textBoxBindingNullable_Format);
+                b.Parse -= new ConvertEventHandler(textBoxBindingNullable_Parse);
+                b.Format += new ConvertEventHandler(textBoxBindingNullable_Format);
+                b.Parse += new ConvertEventHandler(textBoxBindingNullable_Parse);
+                this.textBoxPhi.DataBindings.Add(b);
+                //this.textBoxPhi.DataBindings.Add("Text", currentParametricCurveRecruit, "phi", true, 
+                //    DataSourceUpdateMode.OnValidation, string.Empty);
                 this.textBoxLastResidual.DataBindings.Add("Text", currentParametricCurveRecruit, "lastResidual", true,
-                    DataSourceUpdateMode.OnPropertyChanged);
+                    DataSourceUpdateMode.OnValidation, string.Empty);
             }
 
             base.SetParametricRecruitmentControls(currentRecruit, panelRecruitModelParameter);
         }
+
+        
+        /// <summary>
+        /// How the nullable data object gets formatted to the control is bounded to.  Object to control.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void textBoxBindingNullable_Format(object sender, ConvertEventArgs e)
+        {
+            Binding b = sender as Binding;
+            if (b != null)
+            {
+                TextBox ctlTxt = (b.Control as TextBox);
+                if (ctlTxt != null)
+                {
+                    if (e.Value == null)
+                    {
+                        e.Value = ctlTxt.Text;
+                    }
+                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// How the control's formatted nullable value is stored to that object its bounded to. Control to Object.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void textBoxBindingNullable_Parse(object sender, ConvertEventArgs e)
+        {
+            Binding b = sender as Binding;
+            if (b != null)
+            {
+                TextBox ctlTxt = b.Control as TextBox;
+                if (ctlTxt != null)
+                {
+                    double val;
+                    if (Double.TryParse(e.Value.ToString(), out val))
+                    {
+                        if (string.IsNullOrWhiteSpace(ctlTxt.Text))
+                        {
+                            ctlTxt.Undo();
+                        }
+                        else
+                        {
+                            e.Value = new double?(val);
+                        }            
+                    }
+                    else
+                    {
+                        ctlTxt.Undo();
+                    }
+                }
+            }
+        }
+
     }
 }
