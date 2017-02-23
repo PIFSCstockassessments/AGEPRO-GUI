4f61981d235b6445b276c445956fb167263a8691
Eric Fletcher <eric.fletcher@noaa.gov>
2017-02-22 14:12:57 -1000

    Added AgeproBinding and NftTextBox

---
+++ AgeproBinding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Nmfs.Agepro.Gui
+{
+    public interface IValueConverter
+    {
+        object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+    }
+
+    public class AgeproBinding : Binding
+    {
+        public AgeproBinding(string propertyName, object dataSource, string dataMember) 
+            : base(propertyName, dataSource, dataMember, true)
+        {
+
+        }
+
+        
+    }
+}
---
+++ NftTextBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nmfs.Agepro.Gui
+{
+    public class NftTextBox : TextBox
+    {
+
+        protected override void OnValidating(System.ComponentModel.CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.Text))
+            {
+                return;
+            }
+            base.OnValidating(e);
+        }
+    }
+}
