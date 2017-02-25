f684cad7fe73bc030a623d05aca928fe21c8c3ae
Eric Fletcher <eric.fletcher@noaa.gov>
2017-02-24 15:04:43 -1000

    Added NullableDoubleStringValueConverter

--- AgeproBinding.cs
+++ AgeproBinding.cs
@@ -16,12 +16,52 @@ namespace Nmfs.Agepro.Gui
 
     public class AgeproBinding : Binding
     {
-        public AgeproBinding(string propertyName, object dataSource, string dataMember) 
-            : base(propertyName, dataSource, dataMember, true)
+        private readonly IValueConverter _converter;
+        private readonly object _converterParameter;
+        private readonly CultureInfo _converterCulture;
+
+
+        public AgeproBinding(string propertyName, object dataSource, string dataMember, IValueConverter valueConverter, object converterParameter = null)
+            : this(propertyName, dataSource, dataMember, valueConverter, System.Threading.Thread.CurrentThread.CurrentUICulture, converterParameter)
         {
 
         }
 
-        
+        public AgeproBinding(string propertyName, object dataSource, string dataMember, IValueConverter valueConverter, CultureInfo culture, object converterParameter = null) 
+            : base(propertyName, dataSource, dataMember, true)
+        {
+            this._converter = valueConverter;
+            this._converterCulture = culture;
+            this._converterParameter = converterParameter;
+        }
+
+        protected override void OnFormat(ConvertEventArgs cevent)
+        {
+            if (this._converter != null)
+            {
+                var convertedValue = this._converter.Convert(cevent.Value, cevent.DesiredType, this._converterParameter, this._converterCulture);
+                cevent.Value = convertedValue;
+            }
+            else
+            {
+                base.OnFormat(cevent);
+            }
+            
+        }
+
+
+        protected override void OnParse(ConvertEventArgs cevent)
+        {
+            if (this._converter != null)
+            {
+                var valueFromCtl = this._converter.ConvertBack(cevent.Value, cevent.DesiredType, this._converterParameter, this._converterCulture);
+                cevent.Value = valueFromCtl;
+            }
+            else
+            {
+                base.OnParse(cevent);
+            }
+            
+        }
     }
 }
---
+++ NullableDoubleStringValueConverter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nmfs.Agepro.Gui
+{
+    public class NullableDoubleStringValueConverter : IValueConverter
+    {
+        //custom-winforms-data-binding-with-converter-not-working-on-nullable-type-double
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value != null ? ((double)value).ToString(culture) : string.Empty;
+        }
+
+        public Object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var textVal = value != null ? ((string)value).Trim() : null;
+            return !string.IsNullOrEmpty(textVal) ? (object)double.Parse(textVal, System.Globalization.NumberStyles.Any, culture) : null;
+            
+        }
+    }
+
+    public class ScaleFactorStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((double)value > 0)
+            {
+                return ((double)value).ToString(culture);
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Data object returned invaild Scale Factor. Default to 1.");
+                return 1;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                double factorVal;
+                string textVal = ((string)value).Trim();
+
+                if (double.TryParse(textVal, out factorVal))
+                {
+                    if (factorVal > 0)
+                    {
+                        return (object)factorVal;
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show(parameter.ToString() 
+                            + " should be a positive, non-zero number.",
+                            "AGEPRO",
+                            System.Windows.Forms.MessageBoxButtons.OK,
+                            System.Windows.Forms.MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show(parameter.ToString() 
+                        + " is not a vaild double numeric data type.",
+                        "AGEPRO",
+                        System.Windows.Forms.MessageBoxButtons.OK,
+                        System.Windows.Forms.MessageBoxIcon.Error);                 
+                }
+            }
+            return null; //failstate
+        }
+    }
+}
