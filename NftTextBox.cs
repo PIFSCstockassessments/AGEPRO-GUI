9c3d76ff83bca6927d115ae92db776da72cf797d
Eric Fletcher <eric.fletcher@noaa.gov>
2017-03-06 10:11:01 -1000

    Implement prevValidValue

--- NftTextBox.cs
+++ NftTextBox.cs
@@ -10,13 +10,31 @@ namespace Nmfs.Agepro.Gui
     public class NftTextBox : TextBox
     {
 
+        private string prevValidValue = string.Empty;
+        private string paramName { get; set; }
+
+        public NftTextBox()
+        {
+            this.CausesValidation = true;
+        }
+
+
+
         protected override void OnValidating(System.ComponentModel.CancelEventArgs e)
         {
             if (string.IsNullOrWhiteSpace(this.Text))
             {
+                e.Cancel = true;
                 return;
             }
             base.OnValidating(e);
         }
+
+        protected override void OnValidated(EventArgs e)
+        {
+            this.prevValidValue = this.Text;
+            base.OnValidated(e);
+        }
+          
     }
 }
