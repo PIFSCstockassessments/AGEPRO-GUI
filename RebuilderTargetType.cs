47703dd98fca00c5b2dd4abae3f40434fb33598d
Eric Fletcher <eric.fletcher@noaa.gov>
2017-03-10 09:45:26 -1000

    Documentation

--- RebuilderTargetType.cs
+++ RebuilderTargetType.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Nmfs.Agepro.Gui
 {
+    /// <summary>
+    /// A BindingList object to populate the "Rebulider Target Type" Combo Box. 
+    /// </summary>
     class RebuilderTargetType : AgeproPropertyChanged
     {
 
