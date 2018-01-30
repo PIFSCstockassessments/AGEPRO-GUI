a247fc38c1c0667cd32668fdbde707ef62c020c9
Eric Fletcher <eric.fletcher@noaa.gov>
2018-01-30 11:52:10 -1000

    GetAgeproUserDataPath Documentation
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- Util.cs
+++ Util.cs
@@ -8,12 +8,18 @@ namespace Nmfs.Agepro.Gui
 {
     class Util
     {
+        /// <summary>
+        /// Platform-indpendnent method to get AGEPRO subdirectory to user's 'My Documents' path. If
+        /// the subdirectory doesn't exist may create one. 
+        /// </summary>
+        /// <returns>Filename string path of AGEPRO subdirectory.</returns>
         static public string GetAgeproUserDataPath()
         {
             string appDir = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             appDir = System.IO.Path.Combine(appDir, "AGEPRO");
             if (!System.IO.Directory.Exists(appDir))
             {
+                //TODO: ASK USER TO CREATE DIRECTORY
                 System.IO.Directory.CreateDirectory(appDir);
             }
             return appDir;
