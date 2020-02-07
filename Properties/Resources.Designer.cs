860e58b187a71a5f6d3f7e14dad44d38beecaa50
Eric Fletcher <eric.fletcher@noaa.gov>
2020-02-07 11:55:02 -1000

    Embed & access NOAA logoPictureBox_image from Resources file
    Signed-off-by: Eric Fletcher <eric.fletcher@noaa.gov>

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Nmfs.Agepro.Gui.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -57,6 +57,36 @@ namespace Nmfs.Agepro.Gui.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon AgeproIcon {
+            get {
+                object obj = ResourceManager.GetObject("AgeproIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap logoPictureBox {
+            get {
+                object obj = ResourceManager.GetObject("logoPictureBox", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap logoPictureBox_Image {
+            get {
+                object obj = ResourceManager.GetObject("logoPictureBox_Image", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
