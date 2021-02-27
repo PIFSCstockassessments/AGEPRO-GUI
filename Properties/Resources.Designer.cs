a49ea6e8dd8dbbdcd5abd611230feac11dcdecfd
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-02-26 14:02:45 -1000

    Update NOAA Fisheries Branding
    Replace About Dialog NOAA Logo Picture Box w/ new one.

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace Nmfs.Agepro.Gui.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap noaa_fisheries_pictureBoxImage {
+            get {
+                object obj = ResourceManager.GetObject("noaa_fisheries_pictureBoxImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
