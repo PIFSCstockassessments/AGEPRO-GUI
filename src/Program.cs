da83caff48c668aef02fc3a34bf04500be66fe3e
efletcherPIFSC <37851243+efletcherPIFSC@users.noreply.github.com>
2021-03-23 14:30:46 -1000

    Moved Program.cs in src directory

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Nmfs.Agepro.CoreLib;
 
 namespace Nmfs.Agepro.Gui
 {
-    static class Program
+  static class Program
     {
         /// <summary>
         /// The main entry point for the application.
