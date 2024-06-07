8d7ea05a97b1119446e1f2bbbce82407e75bb359
Eric Fletcher <37851243+efletcherPIFSC@users.noreply.github.com>
2024-06-06 16:34:23 -1000

    Added Stock Summary Flag Enum

---
+++ src/ui/formAgepro/options/StockSummaryFlag.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Nmfs.Agepro.Gui.src.ui.formAgepro.options
+{
+  public enum StockSummaryFlag
+  {
+    None,
+    OnlySummary,
+    SummaryPlusAuxFiles_ExcludeStockAgeAuxFile,
+    SummaryPlusAllAuxFiles
+
+  }
+}
